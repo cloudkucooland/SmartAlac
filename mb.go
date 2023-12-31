230c28811abd969f141fbf7024666bfbd1b8c242
Scot Bontrager <cloudkucooland@gmail.com>
2023-12-30 18:43:08 -0600

    just banging away at this

--- mb.go
+++ mb.go
@@ -1,83 +1,21 @@
 package sa
 
 import (
+	"fmt"
+	"log"
+	"strings"
+
 	"github.com/Sorrow446/go-mp4tag"
 	"github.com/kr/pretty"
 	"github.com/michiwend/gomusicbrainz"
-	"log"
 	// if the caches are too big, try one of these (or others)
 	// git.mills.io/prologic/bitcask
 	// github.com/dgraph-io/ristretto
 	// github.com/peterbourgon/diskv
 )
 
-/* go-mp4tag.MP4Tag:
-   Album:What Girls Want
-   AlbumSort:
-   AlbumArtist:Material Issue
-   AlbumArtistSort:
-   Artist:Material Issue Artist
-   Sort:
-   BPM:-1
-   Comment:alternative rock
-   Composer:
-   ComposerSort:
-   Conductor:
-   Copyright:
-   CustomGenre:Alternative Rock
-   Date:
-   Description:
-   Director:
-   DiscNumber:1
-   DiscTotal:1
-   Genre:0
-   ItunesAdvisory:0
-   ItunesAlbumID:-1
-   ItunesArtistID:-1
-   Lyrics:
-   Narrator:
-   Pictures:[]
-   Publisher:
-   Title:What Girls Want (radio mix)
-   TitleSort:
-   TrackNumber:1
-   TrackTotal:3
-   Year:1992
-
-   --- Custom ---
-   ARTISTS:Material Issue
-   BARCODE:314512333243
-   CATALOGNUMBER:CDP 685
-   Country:United States
-   KEY:A
-   LABEL:Mercury Records
-   LANGUAGE:eng
-   MEDIA:CD
-   MOOD:bright; happy; relaxed
-   MusicBrainz Album Artist Id:35290bf2-aa8e-412d-8f4e-0b3362044958
-   MusicBrainz Album Id:bf7cb22d-a15a-4ce2-a5ff-b0c4f8608f2e
-   MusicBrainz Album Release Country:US
-   MusicBrainz Album Type:single
-   MusicBrainz Artist Id:35290bf2-aa8e-412d-8f4e-0b3362044958
-   MusicBrainz Disc Id:YeZdB4dY7lQIC0ZSgkbe2R4HXAs-
-   MusicBrainz Release Group Id:c2912250-8518-45eb-bf43-5d57d8607d82
-   MusicBrainz Release Track Id:5bbd0145-a8d8-3321-8442-638690620ae7
-   MusicBrainz Track Id:5bbd0145-a8d8-3321-8442-638690620ae7
-   ORIGINALDATE:1992
-   ORIGINALYEAR:1992
-   RELEASESTATUS:promotion
-   SCRIPT:Latn
-   URL_DISCOGS_ARTIST_SITE:http://discogs.com/artist/261512
-   URL_DISCOGS_RELEASE_SITE:https://www.discogs.com/release/2653425
-   URL_LYRICS_SITE:http://lyricsfly.com/search/correction.php?8ebcad6785&id=621629
-*/
-
 var client *gomusicbrainz.WS2Client
-
 var releases map[string]*gomusicbrainz.Release
-
-// var artists map[string]*gomusicbrainz.Artist
-// var labels map[string]*gomusicbrainz.Label
 
 func init() {
 	client, _ = gomusicbrainz.NewWS2Client(
@@ -87,24 +25,17 @@ func init() {
 		"http://github.com/cloudkucooland/SmartAlac")
 
 	releases = make(map[string]*gomusicbrainz.Release)
-	// artists = make(map[string]*gomusicbrainz.Artist)
-	// labels = make(map[string]*gomusicbrainz.Label)
 }
 
 func updateFromMB(in *mp4tag.MP4Tags) (*mp4tag.MP4Tags, bool, error) {
-	out := mp4tag.MP4Tags{}
-	// constants
-	out.ItunesAdvisory = 0
-	out.ItunesAlbumID = -1
-	out.ItunesArtistID = -1
+	out := mp4tag.MP4Tags{
+		ItunesAdvisory: 0,
+		ItunesAlbumID:  -1,
+		ItunesArtistID: -1,
+	}
 
-	// things which we don't touch but do want to preserve
-	out.BPM = in.BPM
-	out.Lyrics = in.Lyrics
-
-	// copy over custom items determined eleswhere
 	out.Custom = make(map[string]string)
-	copyCustoms := []string{"KEY", "MOOD"}
+	copyCustoms := []string{"KEY", "MOOD", "URL_LYRICS_SITE", "VINYLDIGITIZER", "URL_DISCOGS_ARTIST_SITE"}
 	for _, v := range copyCustoms {
 		if _, ok := in.Custom[v]; ok {
 			out.Custom[v] = in.Custom[v]
@@ -125,9 +56,8 @@ func updateFromMB(in *mp4tag.MP4Tags) (*mp4tag.MP4Tags, bool, error) {
 
 	release, ok := releases[releaseid]
 	if !ok {
-		log.Printf("querying for %s\n\n", releaseid)
 		var err error
-		release, err = client.LookupRelease(gomusicbrainz.MBID(releaseid), "artist-credits", "recordings", "release-groups", "media", "isrcs", "release-rels", "release-group-rels", "url-rels")
+		release, err = client.LookupRelease(gomusicbrainz.MBID(releaseid), "artist-credits", "recordings", "release-groups", "media", "isrcs", "release-rels", "release-group-rels", "url-rels", "labels")
 		if err != nil {
 			log.Println(err.Error())
 			return in, false, err
@@ -138,35 +68,60 @@ func updateFromMB(in *mp4tag.MP4Tags) (*mp4tag.MP4Tags, bool, error) {
 
 	// per release items
 	out.Album = release.Title
-	// out.AlbumSort =
+	out.AlbumSort = release.Title
 	out.AlbumArtist = fmtArtistCredit(release.ArtistCredit.NameCredits)
-	// out.AlbumArtistSort =
+	out.AlbumArtistSort = fmtArtistCreditSort(release.ArtistCredit.NameCredits)
+	out.BPM = in.BPM
 	out.DiscNumber = in.DiscNumber
-	// out.DiscTotal = release.Mediums.Length()
+	out.DiscTotal = getMediumCount(release)
 	// out.Sort:
 	out.TrackTotal = getMediumTrackCount(release, in.DiscNumber)
-	// out.Publisher = release.
+	// out.Publisher =
 	out.Year = int32(release.ReleaseGroup.FirstReleaseDate.Year())
 
 	// per track items
+	medium := getMedium(release, in.DiscNumber)
 	track := getTrack(release, gomusicbrainz.MBID(tid))
 	out.Artist = fmtArtistCredit(track.Recording.ArtistCredit.NameCredits)
-	/*   out.Comment:alternative rock
-	out.Composer:
-	out.ComposerSort:
-	out.Conductor:
-	out.Copyright:
-	out.CustomGenre:Alternative Rock
-	out.Date:
-	out.Description:
-	out.Director:
-	out.Genre:0
-	out.Lyrics:
-	out.Narrator:
-	out.Title:What Girls Want (radio mix)
-	out.TitleSort:
-	*/
+	out.Comment = in.Comment
+	// out.Composer:
+	// out.ComposerSort:
+	// out.Conductor:
+	// out.Copyright:
+
+	out.CustomGenre = in.CustomGenre
+	// out.Date:
+	// out.Description:
+	// out.Director:
+	// out.Genre:0
+	out.Lyrics = in.Lyrics
+	// out.Narrator:
+	out.Title = track.Recording.Title
+	// out.TitleSort:
 	out.TrackNumber = in.TrackNumber
+
+	out.Custom["ARTISTS"] = fmtArtistList(release.ArtistCredit.NameCredits)
+	out.Custom["BARCODE"] = release.Barcode
+	out.Custom["CATALOGNUMBER"] = fmtCatalogNumber(release.LabelInfos)
+	out.Custom["Country"] = release.CountryCode // need to convert to country name
+	out.Custom["LABEL"] = fmtLabel(release.LabelInfos)
+	out.Custom["LANGUAGE"] = release.TextRepresentation.Language
+	out.Custom["MEDIA"] = medium.Format
+	out.Custom["MusicBrainz Album Artist Id"] = joinArtistIDs(release.ArtistCredit.NameCredits)
+	out.Custom["MusicBrainz Album Id"] = releaseid
+	out.Custom["MusicBrainz Album Release Country"] = release.CountryCode
+	out.Custom["MusicBrainz Album Type"] = strings.ToLower(release.ReleaseGroup.Type)
+	out.Custom["MusicBrainz Artist Id"] = joinArtistIDs(track.Recording.ArtistCredit.NameCredits)
+	// out.Custom["MusicBrainz Disc Id"] :YeZdB4dY7lQIC0ZSgkbe2R4HXAs-
+	out.Custom["MusicBrainz Release Group Id"] = string(release.ReleaseGroup.ID)
+	out.Custom["MusicBrainz Release Track Id"] = tid
+	out.Custom["MusicBrainz Track Id"] = tid
+	out.Custom["ORIGINALDATE"] = fmt.Sprintf("%d", release.ReleaseGroup.FirstReleaseDate.Year())
+	out.Custom["ORIGINALYEAR"] = fmt.Sprintf("%d", release.ReleaseGroup.FirstReleaseDate.Year())
+	out.Custom["RELEASESTATUS"] = strings.ToLower(release.Status)
+	out.Custom["SCRIPT"] = release.TextRepresentation.Script
+
+	// out.Custom["URL_DISCOGS_RELEASE_SITE"] = release.Relations.Url.RelationAbstract.Target
 
 	log.Printf("%# v\n", pretty.Formatter(pretty.Diff(in, &out)))
 
@@ -174,22 +129,70 @@ func updateFromMB(in *mp4tag.MP4Tags) (*mp4tag.MP4Tags, bool, error) {
 }
 
 func getMediumTrackCount(r *gomusicbrainz.Release, discnumber int16) int16 {
+	for _, m := range r.Mediums {
+		if int16(m.Position) == discnumber {
+			return int16(len(m.Tracks))
+		}
+	}
 	return 0
+}
+
+func getMediumCount(r *gomusicbrainz.Release) int16 {
+	return int16(len(r.Mediums))
+}
+
+func getMedium(r *gomusicbrainz.Release, discnumber int16) *gomusicbrainz.Medium {
+	for _, m := range r.Mediums {
+		if int16(m.Position) == discnumber {
+			return m
+		}
+	}
+	return nil
 }
 
 func fmtArtistCredit(a []gomusicbrainz.NameCredit) string {
 	var s string
-	for _, v := range a {
+	for k, v := range a {
+		if k > 0 {
+			s += " "
+		}
 		s += v.Artist.Name
 	}
-	log.Println(s)
+	// log.Printf("fmtArtistCredit: %s", s)
+	return s
+}
+
+func fmtArtistList(a []gomusicbrainz.NameCredit) string {
+	var s string
+	for k, v := range a {
+		if k > 0 {
+			s += ", "
+		}
+		s += v.Artist.Name
+	}
+	// log.Printf("fmtArtistList: %s", s)
 	return s
 }
 
 func fmtArtistCreditSort(a []gomusicbrainz.NameCredit) string {
 	var s string
-	for _, v := range a {
+	for k, v := range a {
+		if k > 0 {
+			s += ", "
+		}
 		s += v.Artist.SortName
+	}
+	// log.Printf("fmtArtistCreditSort: %s", s)
+	return s
+}
+
+func joinArtistIDs(a []gomusicbrainz.NameCredit) string {
+	var s string
+	for k, v := range a {
+		if k > 0 {
+			s += ","
+		}
+		s += string(v.Artist.ID)
 	}
 	return s
 }
@@ -204,4 +207,26 @@ func getTrack(r *gomusicbrainz.Release, trackID gomusicbrainz.MBID) *gomusicbrai
 	}
 	log.Println("unable to find matching trackID")
 	return nil
+}
+
+func fmtCatalogNumber(l []gomusicbrainz.LabelInfo) string {
+	var s string
+	for i, li := range l {
+		if i > 0 {
+			s += ", "
+		}
+		s += li.CatalogNumber
+	}
+	return s
+}
+
+func fmtLabel(l []gomusicbrainz.LabelInfo) string {
+	var s string
+	for i, li := range l {
+		if i > 0 {
+			s += ", "
+		}
+		s += li.Label.Name
+	}
+	return s
 }
